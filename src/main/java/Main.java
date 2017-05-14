8d09af39888f15ac6886603d95ea5c7d2910cfa3
Miguel Salcedo <msalcedo92@gmail.com>
2017-05-13 21:52:01 -0700

    RB-6 Create Java Docker Container that calls the Motor Service Image, then terminates.

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public final class Main {
+    public static void main(String[] arguments) throws Exception {
+        final URL motorService = new URL("http://motor-service/");
+        final URLConnection urlConnection = motorService.openConnection();
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+        reader.lines().forEach(System.out::println);
+        reader.close();
+    }
+}
