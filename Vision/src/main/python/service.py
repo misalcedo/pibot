b7a600be09df0625afbb540dde1ec4eed57dc1ca
Miguel Salcedo <msalcedo92@gmail.com>
2017-07-25 20:16:01 -0700

    Add vision docker image to project. Rename motor-service to motor. Rename Robot to Hub.

---
+++ Vision/src/main/python/service.py
@@ -0,0 +1,50 @@
+import atexit
+
+from Adafruit_MotorHAT import Adafruit_MotorHAT
+from flask import Flask, request
+
+# create a default object, no changes to I2C address or frequency
+LOCATION_TO_INDEX = {"BACK_RIGHT": 1, "BACK_LEFT": 2}
+COMMAND_MAP = {
+    "FORWARD": Adafruit_MotorHAT.FORWARD,
+    "BACKWARD": Adafruit_MotorHAT.BACKWARD,
+    "BRAKE": Adafruit_MotorHAT.BRAKE,
+    "RELEASE": Adafruit_MotorHAT.RELEASE
+}
+
+motor_hat = Adafruit_MotorHAT()
+
+
+# recommended for auto-disabling motors on shutdown!
+def turn_off_motors():
+    motor_hat.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+    motor_hat.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+    motor_hat.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    motor_hat.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+
+atexit.register(turn_off_motors)
+
+app = Flask(__name__)
+
+
+def update_motor(key, value):
+    index = LOCATION_TO_INDEX[key]
+    command = value["command"]
+    speed = value["speed"]
+
+    app.logger.debug("Updating %s motor with index %d to speed: %d, command: %s", key, index, speed, command)
+
+    motor = motor_hat.getMotor(index)
+    motor.run(COMMAND_MAP[command])
+    motor.setSpeed(speed)
+
+
+@app.route('/motors', methods=['PUT'])
+def drive():
+    for key, value in request.json["motors"].items():
+        update_motor(key, value)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
