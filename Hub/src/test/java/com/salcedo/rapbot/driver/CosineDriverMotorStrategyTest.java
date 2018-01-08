487cee61a13f14a4646f969f3f08269379c14f54
Miguel Salcedo <msalceod92@gmail.com>
2018-01-07 21:28:48 -0800

    Add tests for driver motor strategy

--- Hub/src/test/java/com/salcedo/rapbot/driver/CosineDriverMotorStrategyTest.java
+++ Hub/src/test/java/com/salcedo/rapbot/driver/CosineDriverMotorStrategyTest.java
@@ -1,6 +1,5 @@
 package com.salcedo.rapbot.driver;
 
-import akka.actor.ActorSystem;
 import com.salcedo.rapbot.locomotion.Command;
 import com.salcedo.rapbot.locomotion.Location;
 import com.salcedo.rapbot.locomotion.Motor;
@@ -8,6 +7,10 @@ import com.salcedo.rapbot.locomotion.MotorRequest;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.salcedo.rapbot.locomotion.Command.BACKWARD;
+import static com.salcedo.rapbot.locomotion.Command.FORWARD;
+import static com.salcedo.rapbot.locomotion.Location.BACK_LEFT;
+import static com.salcedo.rapbot.locomotion.Location.BACK_RIGHT;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
@@ -30,69 +33,78 @@ public class CosineDriverMotorStrategyTest {
                 15
         );
         final MotorRequest motorRequest = motorStrategy.drive(driveState);
-        final Motor leftMotor = motorRequest.getMotor(Location.BACK_LEFT).orElseThrow(IllegalStateException::new);
-        final Motor rightMotor = motorRequest.getMotor(Location.BACK_RIGHT).orElseThrow(IllegalStateException::new);
+        final Motor leftMotor = motorRequest.getMotor(BACK_LEFT).orElseThrow(IllegalStateException::new);
+        final Motor rightMotor = motorRequest.getMotor(BACK_RIGHT).orElseThrow(IllegalStateException::new);
 
         assertThat(leftMotor.getCommand(), is(equalTo(rightMotor.getCommand())));
         assertThat(rightMotor.getSpeed(), is(greaterThan(leftMotor.getSpeed())));
     }
 
     @Test
-    public void driveV2() {
-        final int throttle = 255;
-
-        System.out.println("Degree\tOpp\tLeft\tRight\tCommand");
-        for (int i = 0; i <= 180; i += 15) {
-            final MotorRequest motorRequest = motorStrategy.drive(createDriveState(255, i));
-
-            System.out.printf(
-                    "%d\t%f\t%d\t%d\t%s\n",
-                    i,
-                    Math.sin(Math.toRadians(i)) * throttle,
-                    motorRequest
-                            .getMotor(Location.BACK_LEFT)
-                            .map(Motor::getSpeed)
-                            .orElse(0),
-                    motorRequest
-                            .getMotor(Location.BACK_RIGHT)
-                            .map(Motor::getSpeed)
-                            .orElse(0),
-                    motorRequest
-                            .getMotor(Location.BACK_RIGHT)
-                            .map(Motor::getCommand)
-                            .orElse(Command.BRAKE)
-            );
-        }
-
-        System.out.println("Degree\tOpp\tLeft\tRight\tCommand");
-        for (int i = 180; i <= 360; i += 15) {
-            final MotorRequest motorRequest = motorStrategy.drive(createDriveState(255, i));
-            System.out.printf(
-                    "%d\t%f\t%d\t%d\t%s\n",
-                    i,
-                    Math.sin(Math.toRadians(i)) * throttle,
-                    motorRequest
-                            .getMotor(Location.BACK_LEFT)
-                            .map(Motor::getSpeed)
-                            .orElse(0),
-                    motorRequest
-                            .getMotor(Location.BACK_RIGHT)
-                            .map(Motor::getSpeed)
-                            .orElse(0),
-                    motorRequest
-                            .getMotor(Location.BACK_RIGHT)
-                            .map(Motor::getCommand)
-                            .orElse(Command.BRAKE)
-            );
-        }
+    public void driveForwardLeft() {
+        assertTurningDrive(120, FORWARD, BACK_RIGHT, BACK_LEFT);
     }
 
-    private DriveState createDriveState(final int throttle, final int orientation) {
+    private void assertTurningDrive(
+            final int orientation,
+            final Command command,
+            final Location slowerLocation,
+            final Location fasterLocation
+    ) {
+        final DriveState driveState = createDriveState(orientation);
+        final MotorRequest motorRequest = motorStrategy.drive(driveState);
+        final Motor slowerMotor = motorRequest.getMotor(slowerLocation).orElseThrow(IllegalStateException::new);
+        final Motor fasterMotor = motorRequest.getMotor(fasterLocation).orElseThrow(IllegalStateException::new);
+
+        assertThat(slowerMotor.getCommand(), is(equalTo(fasterMotor.getCommand())));
+        assertThat(slowerMotor.getCommand(), is(equalTo(command)));
+        assertThat(fasterMotor.getSpeed(), is(greaterThan(slowerMotor.getSpeed())));
+        assertThat(slowerMotor.getSpeed(), is(greaterThan(0)));
+    }
+
+    private DriveState createDriveState(final int orientation) {
         return new DriveState(
                 new OpenClosedRange(true, true, 0, 255),
                 new OpenClosedRange(true, true, 0, 360),
-                throttle,
+                255,
                 orientation
         );
+    }
+
+    @Test
+    public void driveBackwardLeft() {
+        assertTurningDrive(210, BACKWARD, BACK_LEFT, BACK_RIGHT);
+    }
+
+    @Test
+    public void driveForwardRight() {
+        assertTurningDrive(30, FORWARD, BACK_LEFT, BACK_RIGHT);
+    }
+
+    @Test
+    public void driveBackwardRight() {
+        assertTurningDrive(300, BACKWARD, BACK_RIGHT, BACK_LEFT);
+    }
+
+    @Test
+    public void driveForward() {
+        assertStraightDrive(90, FORWARD);
+    }
+
+    @Test
+    public void driveBackward() {
+        assertStraightDrive(270, BACKWARD);
+    }
+
+    private void assertStraightDrive(final int orientation, final Command command) {
+        final DriveState driveState = createDriveState(orientation);
+        final MotorRequest motorRequest = motorStrategy.drive(driveState);
+        final Motor leftMotor = motorRequest.getMotor(BACK_LEFT).orElseThrow(IllegalStateException::new);
+        final Motor rightMotor = motorRequest.getMotor(BACK_RIGHT).orElseThrow(IllegalStateException::new);
+
+        assertThat(leftMotor.getCommand(), is(equalTo(rightMotor.getCommand())));
+        assertThat(leftMotor.getCommand(), is(equalTo(command)));
+        assertThat(rightMotor.getSpeed(), is(equalTo(leftMotor.getSpeed())));
+        assertThat(leftMotor.getSpeed(), is(equalTo(driveState.getThrottle())));
     }
 }
