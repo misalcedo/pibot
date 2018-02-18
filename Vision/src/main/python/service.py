034a4fc8fda7892aa35e55feb3b253b5b030dd51
Miguel Salcedo <msalceod92@gmail.com>
2018-02-17 22:44:30 -0800

    Revert to non-flask vision implementation. Need to determine how to optimize the CPU usage of the camera.

--- Vision/src/main/python/service.py
+++ Vision/src/main/python/service.py
@@ -1,54 +1,39 @@
-from flask import Flask, Response
-from io import BytesIO
-
-from camera import create_camera
+from picamera import PiCamera
 from mjpeg import FrameSplitter
-
-PAGE = """\
-    <html>
-        <head>
-        <title>RapBot MJPEG Stream</title>
-        </head>
-        <body>
-            <h1>RapBot MJPEG Stream</h1>
-            <img src="stream.mjpg"/>
-        </body>
-    </html>
-    """.encode('utf-8')
-
-app = Flask(__name__)
-video_output = FrameSplitter()
-camera = create_camera(video_output)
+from stream import StreamingServer, BaseStreamingHandler
 
 
-@app.route("/")
-def root():
-    return PAGE
+class StreamingHandler(BaseStreamingHandler):
+    def send_frame(self):
+        camera.capture(self.wfile, 'jpeg', use_video_port=True)
+
+    def send_frames(self):
+        try:
+            video_output.truncate()
+
+            while True:
+                frame = video_output.read()
+                if frame:
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+        except Exception as e:
+            self.log_message('Removed streaming client %s: %s', self.client_address, str(e))
 
 
-@app.route("/still.jpg")
-def still():
-    def generate(buffer):
-        camera.capture(buffer, format='jpeg', use_video_port=True)
+if __name__ == '__main__':
+    with PiCamera(framerate=24) as camera:
+        camera.hflip = True
+        camera.vflip = True
+        camera.start_preview()
+        video_output = FrameSplitter()
+        camera.start_recording(video_output, format='mjpeg')
 
-        buffer.seek(0)
-
-        yield buffer.read()
-
-    return Response(generate(BytesIO()), mimetype='image/jpeg')
-
-
-@app.route("/stream.mjpg")
-def stream():
-    def generate():
-        video_output.truncate()
-
-        while True:
-            frame = video_output.read()
-            if frame:
-                yield b'--FRAME\r\n'
-                yield b'Content-Type: image/jpeg\r\n\r\n'
-                yield frame
-                yield b'\r\n'
-
-    return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=FRAME')
+        try:
+            server = StreamingServer(('', 80), StreamingHandler)
+            server.serve_forever()
+        finally:
+            camera.stop_recording()
