3b6bb0afc44dc0b15e4cd8575ef7419282eb546a
Miguel Salcedo <msalceod92@gmail.com>
2018-03-05 00:04:11 -0800

    Only make a single motor request at a time.

--- Hub/src/main/scala/com/salcedo/rapbot/motor/MotorActor.scala
+++ Hub/src/main/scala/com/salcedo/rapbot/motor/MotorActor.scala
@@ -5,14 +5,14 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.stream.scaladsl.{JsonFraming, Sink}
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
-import com.salcedo.rapbot.motor.MotorActor.Vehicle
+import com.salcedo.rapbot.motor.MotorActor.{Dequeue, Motor, Vehicle}
 import com.salcedo.rapbot.snapshot.RemoteSnapshot
 import com.salcedo.rapbot.snapshot.SnapshotActor.TakeSubSystemSnapshot
 import net.liftweb.json.Serialization.{read, write}
 import net.liftweb.json._
 import net.liftweb.json.ext.EnumSerializer
 
-import scala.concurrent.Future
+import scala.concurrent.{Future, Promise}
 
 object Location extends Enumeration {
   val BackLeft, BackRight = Value
@@ -28,6 +28,8 @@ object MotorActor {
 
   final case class Motor(speed: Int, command: Command.Value, location: Location.Value)
 
+  final case object Dequeue
+
   def props(uri: Uri): Props = Props(new MotorActor(uri))
 }
 
@@ -39,17 +41,36 @@ class MotorActor(val uri: Uri) extends Actor with RemoteSnapshot with ActorLoggi
   implicit val formats: Formats = DefaultFormats + new EnumSerializer(Location) + new EnumSerializer(Command)
 
   val http = Http(context.system)
+  var promise: Promise[Unit] = Promise().success()
+  var next: Option[Vehicle] = None
 
   override def preStart(): Unit = {
     context.system.eventStream.subscribe(self, classOf[Vehicle])
   }
 
   override def receive: PartialFunction[Any, Unit] = {
-    case vehicle: Vehicle => this.update(vehicle)
+    case vehicle: Vehicle => this.enqueue(vehicle)
+    case Dequeue => this.dequeue()
     case _: TakeSubSystemSnapshot => this.snapshot()
   }
 
+  def enqueue(vehicle: Vehicle): Unit = {
+    next = Some(vehicle)
+    dequeue()
+  }
+
+  def dequeue(): Unit = {
+    if (promise.isCompleted) {
+      promise = Promise()
+
+      next.foreach(update)
+      next = None
+    }
+  }
+
   def update(vehicle: Vehicle): Unit = {
+    promise.future.onComplete(self ! _)
+
     val request = HttpRequest(
       uri = uri.withPath(Uri.Path("/motors")),
       method = HttpMethods.PUT,
@@ -59,12 +80,12 @@ class MotorActor(val uri: Uri) extends Actor with RemoteSnapshot with ActorLoggi
     http.singleRequest(request).flatMap {
       case HttpResponse(StatusCodes.OK, _, entity, _) =>
         entity.dataBytes.map(_.utf8String).runWith(Sink.seq).onComplete(f => log.info("Updated motors: {}", f.get.head))
-
-        entity.discardBytes()
-        Future()
+        promise.success().future
       case HttpResponse(statusCode, _, entity, _) =>
         entity.discardBytes()
-        sys.error(s"Received a response with a status code other than 200 OK. Status: $statusCode")
+        val message = s"Received a response with a status code other than 200 OK. Status: $statusCode"
+        promise.failure(new IllegalStateException(message))
+        sys.error(message)
     }
   }
 
