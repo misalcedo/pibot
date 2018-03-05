26615bea513b0195bba193da0979f4ce38c1ad05
Miguel Salcedo <msalceod92@gmail.com>
2018-03-04 23:45:57 -0800

    Anonymize subsystems from Hub.

--- Hub/src/main/scala/com/salcedo/rapbot/hub/Hub.scala
+++ Hub/src/main/scala/com/salcedo/rapbot/hub/Hub.scala
@@ -2,9 +2,10 @@ package com.salcedo.rapbot.hub
 
 import java.nio.file.{Path, Paths}
 
+import akka.actor.Status.Success
 import akka.actor.{Actor, ActorLogging, ActorRef, Props, Status, Terminated}
 import com.salcedo.rapbot.driver.DriverActor.Drive
-import com.salcedo.rapbot.hub.Hub.{NullSystem, SubSystem, System}
+import com.salcedo.rapbot.hub.Hub.SubSystem
 import com.salcedo.rapbot.motor.MotorActor.Vehicle
 import com.salcedo.rapbot.snapshot.SnapshotActor.Snapshot
 import com.salcedo.rapbot.snapshot.SnapshotTakerActor
@@ -13,11 +14,7 @@ import com.salcedo.rapbot.userinterface.{GraphicalUserInterface, GraphicalUserIn
 
 object Hub {
 
-  sealed trait System
-
-  case class SubSystem(props: Props, name: String) extends System
-
-  case object NullSystem extends System
+  case class SubSystem(props: Props, name: String)
 
   case class SystemState(
                           snapshotId: String,
@@ -28,24 +25,20 @@ object Hub {
                           image: String
                         )
 
-  def props(gui: GraphicalUserInterface, driver: System, vision: System, motor: System): Props = {
-    Props(new Hub(gui, driver, vision, motor))
+  def props(gui: GraphicalUserInterface, subSystems: SubSystem*): Props = {
+    Props(new Hub(gui, subSystems))
   }
 }
 
-class Hub(gui: GraphicalUserInterface, driverSystem: System, visionSystem: System, motorSystem: System) extends Actor with ActorLogging {
-  private val driver = actorFor(driverSystem)
-  private val vision = actorFor(visionSystem)
-  private val motor = actorFor(motorSystem)
+class Hub(gui: GraphicalUserInterface, subSystems: Seq[SubSystem]) extends Actor with ActorLogging {
+  private val children = subSystems.map(actorFor)
   private val snapshot = context.actorOf(SnapshotTakerActor.props, "snapshot")
   private val ui = context.actorOf(GraphicalUserInterfaceActor.props(gui), "gui")
 
   override def preStart(): Unit = {
     context.system.eventStream.subscribe(self, classOf[Snapshot])
 
-    snapshot ! RegisterSubSystem(driver)
-    snapshot ! RegisterSubSystem(vision)
-    snapshot ! RegisterSubSystem(motor)
+    children.foreach(snapshot ! RegisterSubSystem(_))
     snapshot ! TakeSnapshot
   }
 
@@ -54,34 +47,19 @@ class Hub(gui: GraphicalUserInterface, driverSystem: System, visionSystem: Syste
     case snapshot: Snapshot => this.state(snapshot)
   }
 
-  def actorFor(system: System): ActorRef = {
-    system match {
-      case SubSystem(props, name) => context.actorOf(props, name)
-      case NullSystem => context.actorOf(NullActor.props)
-    }
+  def actorFor(system: SubSystem): ActorRef = {
+    context.watch(context.actorOf(system.props, system.name))
   }
 
   private def state(snapshot: Snapshot): Unit = {
-    val drive: Option[Drive] = snapshot.responses.get(driver)
-      .filter(_.isInstanceOf[Status.Success])
-      .map(_.asInstanceOf[Status.Success])
+    val snapshots = snapshot.responses.values
+      .filter(_.isInstanceOf[Success])
+      .map(_.asInstanceOf[Success])
       .map(_.status)
-      .filter(_.isInstanceOf[Drive])
-      .map(_.asInstanceOf[Drive])
 
-    val motorResponse: Option[Vehicle] = snapshot.responses.get(null)
-      .filter(_.isInstanceOf[Status.Success])
-      .map(_.asInstanceOf[Status.Success])
-      .map(_.status)
-      .filter(_.isInstanceOf[Vehicle])
-      .map(_.asInstanceOf[Vehicle])
-
-    val image: Option[Path] = snapshot.responses.get(vision)
-      .filter(_.isInstanceOf[Status.Success])
-      .map(_.asInstanceOf[Status.Success])
-      .map(_.status)
-      .filter(_.isInstanceOf[Path])
-      .map(_.asInstanceOf[Path])
+    val drive = snapshots.find(_.isInstanceOf[Drive]).map(_.asInstanceOf[Drive])
+    val motorResponse: Option[Vehicle] = snapshots.find(_.isInstanceOf[Vehicle]).map(_.asInstanceOf[Vehicle])
+    val image: Option[Path] = snapshots.find(_.isInstanceOf[Path]).map(_.asInstanceOf[Path])
 
     context.system.eventStream.publish(new SnapshotBackedSystemState(
       snapshot.uuid,
@@ -104,7 +82,7 @@ class Hub(gui: GraphicalUserInterface, driverSystem: System, visionSystem: Syste
   }
 
   private def terminate(message: Terminated): Unit = {
-    log.error("Actor terminated: {}. Shutting down system.", message.actor)
+    log.error("Actor terminated: {}.", message.actor)
   }
 }
 
