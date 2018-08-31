d210a9a1d41e432ff1b66c87a95b06a59c8cd865
Miguel Salcedo <msalcedo92@gmail.com>
2018-08-30 21:21:13 -0400

    Added Cluster as an example of how to build a UDP server and client in Python.

---
+++ Cluster/client/src/client.py
@@ -0,0 +1,34 @@
+import asyncio
+
+class EchoClientProtocol:
+    def __init__(self, message, loop):
+        self.message = message
+        self.loop = loop
+        self.transport = None
+
+    def connection_made(self, transport):
+        self.transport = transport
+        print('Send:', self.message)
+        self.transport.sendto(self.message.encode())
+
+    def datagram_received(self, data, address):
+        print("Received:", data.decode())
+
+        print("Close the socket")
+        self.transport.close()
+
+    def error_received(self, exc):
+        print('Error received:', exc)
+
+    def connection_lost(self, exception):
+        print("Socket closed, stop the event loop")
+        loop = asyncio.get_event_loop()
+        loop.stop()
+
+loop = asyncio.get_event_loop()
+message = "Hello World!"
+connect = loop.create_datagram_endpoint(lambda: EchoClientProtocol(message, loop), remote_addr=('server', 80))
+transport, protocol = loop.run_until_complete(connect)
+loop.run_forever()
+transport.close()
+loop.close()
